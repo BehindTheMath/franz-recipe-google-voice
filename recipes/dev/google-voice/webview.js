93303048854fa176c759af1443849c9ad1aba001
Behind The Math <info@behindthemath.io>
2017-12-16 19:56:12 -0500

    Initial commit for Franz 5

---
+++ recipes/dev/google-voice/webview.js
@@ -0,0 +1,25 @@
+module.exports = Franz => {
+    function parseIntOrZero(val) {
+        return isNaN(parseInt(val)) ? 0 : parseInt(val);
+    }
+
+    function getMessages() {
+        const el = document.querySelector('.msgCount');
+        let count;
+
+        if (el) {
+            // Legacy version of Google Voice
+            count = parseIntOrZero(el.innerHTML.replace(/[\(\) ]/gi,""));
+        } else {
+            // Updated version of Google Voice
+            const count_messages = parseIntOrZero(document.querySelector('gv-nav-button[label="Messages"] div[aria-label="Unread count"]').innerHTML);
+            const count_calls = parseIntOrZero(document.querySelector('gv-nav-button[label="Calls"] div[aria-label="Unread count"]').innerHTML);
+            const count_voicemails = parseIntOrZero(document.querySelector('gv-nav-button[label="Voicemail"] div[aria-label="Unread count"]').innerHTML);
+            count = count_messages + count_calls + count_voicemails;
+        }
+
+        Franz.setBadge(count);
+    }
+
+    Franz.loop(getMessages);
+};
