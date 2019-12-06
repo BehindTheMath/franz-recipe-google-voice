709f698560956d83834eec0d041582a828d39e1b
Wesley W. Boynton <wes@boynton.io>
2019-12-06 12:15:59 -0500

    Add override for user-agent
    Avoids "unsupported browser" error after auth

--- recipes/dev/google-voice/index.js
+++ recipes/dev/google-voice/index.js
@@ -1,1 +1,7 @@
-module.exports = Franz => Franz;
+"use strict";
+
+module.exports = Franz => class GoogleVoice extends Franz {
+  overrideUserAgent() {
+    return window.navigator.userAgent.replace(/(Franz|Electron|Chrome|AppleWebKit|Safari)([^\s]+(\s|$))/g, '');
+  }
+}
